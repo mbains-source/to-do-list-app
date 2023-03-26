89871e0008a6f755c1c0c4b542ee775557afaea8
mbains-source <114851491+mbains-source@users.noreply.github.com>
2023-03-26 13:01:48 -0700

    Part 4 JQuery Conversion

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 function newItem(){
 
   //1. Adding a new item to the list of items(JQUERY CODE): 
-  
+
      let li = $('<li></li>');
      let inputValue = $('#input').val();
      li.appendChild(inputValue);
@@ -38,24 +38,13 @@ function newItem(){
        li.classList.add("delete")
      }
 
-   // 4. Reordering the items: 
+   // 4. Reordering the items(JQUERY CODE): 
 
      $('#list').sortable();
   
-  }
+    }
   
 
 
-  //3. Adding a delete button
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
-  
-  //   crossOutButton.on("click", deleteListItem);
-  //   function deleteListItem(){
-  // 		li.addClass("delete")
-  // 	}
-     $('#list').sortable();
-  */
-  
+
   
